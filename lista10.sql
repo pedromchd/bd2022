755cffc7e7401d8a8e2534d299cc8b51972ae4d4
pedromchd <pedromchd@outlook.com.br>
2022-08-02 20:49:55 -0300

    lista10

---
+++ lista10.sql
@@ -0,0 +1,34 @@
+PRAGMA foreign_keys = ON;
+
+DROP TABLE IF EXISTS clientes;
+DROP TABLE IF EXISTS Fones_clientes;
+
+CREATE TABLE clientes (
+  cod integer PRIMARY KEY NOT NULL CHECK (cod > 0),
+  Nome varchar(50) NOT NULL,
+  endereco varchar(50) NOT NULL,
+  tipo_cliente char(1) CHECK (tipo_cliente = 'F' or tipo_cliente = 'J'),
+  RG char(15) UNIQUE,
+  CPF char(11) UNIQUE,
+  CNPJ char(14) UNIQUE,
+  Obs text
+);
+
+INSERT INTO clientes (Nome, endereco, tipo_cliente, RG, CPF, Obs) VALUES (
+  'Brenda',
+  'Hernandes, 715',
+  'F',
+  '8996321455',
+  '44521234788',
+  'AaAaAa'
+);
+
+CREATE TABLE Fones_clientes (
+  cliente integer PRIMARY KEY NOT NULL,
+  num_telefone char(10),
+  FOREIGN KEY (cliente) REFERENCES clientes (cod)
+);
+
+INSERT INTO Fones_clientes (cliente, num_telefone) VALUES (
+  1, '5899442681'
+);
