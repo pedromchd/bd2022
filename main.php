e2fce95dd3cb9143f081847d11211f441b3f1b6f
pedromchd <pedromchd@outlook.com.br>
2022-08-07 12:22:04 -0300

    Update main.php

--- main.php
+++ main.php
@@ -2,7 +2,7 @@
 
 $db = new SQLite3("banco.db");
 $db->exec("PRAGMA foreign_keys = ON");
-$db->exec("INSERT INTO tabela (foo, bar) VALUES ('ABC', '123')");
+$db->exec("INSERT INTO tabela (foo, bar) VALUES ('lorem', 'ipsum')");
 $db->close();
 
 ?>
