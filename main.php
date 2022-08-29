2d477f1a8a8b945b793bdc839e5a52e8b3537783
Pedro Machado <pedromchd@outlook.com.br>
2022-08-29 12:37:34 -0300

    php setup

--- main.php
+++ main.php
@@ -1,8 +1,11 @@
 <?php
 
+// php -c php.ini file.php
+// php -S localhost:8080 -c php.ini file.php
+
 $db = new SQLite3("banco.db");
 $db->exec("PRAGMA foreign_keys = ON");
-$queries = explode(";", "lista10.sql");
+$queries = explode(";", file_get_contents("lista10.sql"));
 foreach($queries as $query) {
   $db->exec($query);
 };
