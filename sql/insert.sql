6058cf98ee80e9b26d3a6ef1134c9e13ded4b922
Pedro Machado <pedromchd@outlook.com.br>
2022-09-04 16:21:05 -0300

    insert

---
+++ sql/insert.sql
@@ -0,0 +1,5 @@
+INSERT INTO clientes (nome, endereco, tipo, RG, CPF, CNPJ, obs) VALUES
+  ('Jeferson Nogueira', 'Búzios, 176', 'F', '7848627234', '26410872022', NULL, NULL),
+  ('Daniela Almeida', 'Januário, 413', 'F', '8942375681', '04897685264', NULL, NULL),
+  ('Ricardo Silva', 'Almirantes, 564', 'F', '3456872940', '15486214893', NULL, NULL)
+;
