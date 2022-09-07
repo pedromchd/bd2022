841888672010cd70c10376a94215c18cb0d77516
Pedro Machado <pedromchd@outlook.com.br>
2022-09-07 12:13:55 -0300

    new indexes

--- php/drop.php
+++ php/drop.php
@@ -3,9 +3,9 @@
 // php -c php.ini file.php
 // php -S localhost:8080 -c php.ini file.php
 
-$db = new SQLite3("../lista" . $_GET["lista"] . "/database.db");
+$db = new SQLite3("../database.db");
 $db->exec("PRAGMA foreign_keys = OFF");
-$queries = explode(";", file_get_contents("../lista" . $_GET["lista"] . "/sql/drop.sql"));
+$queries = explode(";", file_get_contents("../sql/lista" . $_GET["lista"] . "/drop.sql"));
 foreach ($queries as $query) {
   $db->exec($query);
 }
